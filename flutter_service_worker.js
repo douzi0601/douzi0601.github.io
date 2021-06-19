e0a7c02c8b224270cd421199cacd20c0626ca851
douzi0601 <364587974@qq.com>
2021-06-19 20:47:19 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c429443f485da0464539169cbc5c3e8b",
-"/": "c429443f485da0464539169cbc5c3e8b",
-"main.dart.js": "93d8b6edc97a39818cd3458874861951",
+"index.html": "005356dbb8ebde1443a3eec9ac8afc13",
+"/": "005356dbb8ebde1443a3eec9ac8afc13",
+"main.dart.js": "4c6c48937811fceba30b972b6d61dcdc",
 "manifest.json": "ffbb90efa086a7ded134f13afca22c1b",
 "version.json": "e92e2d3506a63c048639789e16d8fb62"
 };
